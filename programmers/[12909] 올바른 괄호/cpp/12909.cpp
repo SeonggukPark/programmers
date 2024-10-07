b27b298de4f2edc6f3026c0b6916bcbb196a00f3
Seongguk Park <126008521+SeonggukPark@users.noreply.github.com>
2024-10-07 23:38:16 +0900

    [programmers] 올바른 괄호 (Time: 0.36ms, Memory: 4.22MB) - by Code-Vault

---
+++ programmers/[12909] 올바른 괄호/cpp/12909.cpp
@@ -0,0 +1,20 @@
+#include<string>
+#include <iostream>
+
+using namespace std;
+
+bool solution(string s){
+    bool answer = true;
+    int cnt = 0;
+    
+    for(char c : s){
+        if(c == '(') cnt++;
+        else{
+            if(cnt == 0) return false;
+            cnt--;
+        }
+    }
+
+    if(cnt != 0) return false;
+    return true;
+}
---
+++ programmers/[12909] 올바른 괄호/cpp/README.md
@@ -0,0 +1,8 @@
+# [12909] 올바른 괄호
+
+[문제 출처](https://school.programmers.co.kr/learn/courses/30/lessons/12909)
+
+## 성능
+
+- Time: 0.36ms
+- Memory: 4.22MB
